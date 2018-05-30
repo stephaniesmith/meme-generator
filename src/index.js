48042ac65977dad0768abba19ebc753cd8115818
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-30 12:40:37 -0700

    fixed typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './componets/App';
+import App from './components/App';
 import './normalize.css';
 import './main.css';
 
