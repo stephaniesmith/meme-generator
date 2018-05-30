7f18cff38188ad5fccaa379264c96161d5b5c2c4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-30 12:01:27 -0700

    working on layout and styling

--- src/componets/App.js
+++ src/componets/App.js
@@ -23,7 +23,8 @@ export default class App extends Component {
             text: 'Cats!',
             color: '#000000',
             fonts,
-            selected: 'Nixie One'
+            selected: 'Nixie One',
+            size: '3'
         }
     }
 
@@ -59,44 +60,46 @@ export default class App extends Component {
         });
       }
 
+    handleSizeChange({ target }) {
+        this.setState({ size: target.value });
+    }
+
     render() {
-        const { image, text, color, fonts, selected } = this.state;
+        const { image, text, color, fonts, selected, size } = this.state;
 
         return (
             <section>
                 <h1>Pictures and Stuff</h1>
                 <fieldset>
-                    <div>
-                        <label>
-                            Image Src:
-                            <input onChange={event => this.handleImageSrc(event)}/>
-                        </label>
+                    <div className="row">
+                        <input onChange={event => this.handleImageSrc(event)} placeholder="Image URL"/>
+                        <span> or </span>
+                        <input
+                            type="file"
+                            onChange={event => this.handleUpload(event)}
+                        />
                     </div>
-                    <div>
-                        <label>
-                            Image:
-                            <input
-                                type="file"
-                                onChange={event => this.handleUpload(event)}
-                            />
-                        </label>
-                    </div>
-                    <div>
+                    <div className="row">
                         <select 
                             value={selected}
                             onChange={event => this.handleFontChange(event)}
                         >
                         {fonts.map(font => <option key={font[0]}> {font[0]}</option>)}
                         </select>
-                    </div>
-                    <div>
+                        <input 
+                            type="range" 
+                            onChange={event => this.handleSizeChange(event)}
+                            min=".5"
+                            max="10"
+                            step="0.01"
+                        />
                         <input
                             type="color"
                             onChange={event => this.handleColorChange(event)}
                         />
                     </div>
                     <div>
-                        <input
+                        <textarea
                             type="text"
                             placeholder="Write you meme here"
                             onChange={event => this.handleTextChange(event)}
@@ -109,7 +112,7 @@ export default class App extends Component {
                     </div> */}
                     </fieldset>
                 <div ref={node => this.imageExport = node}>
-                    <h1 style={{ color, fontFamily: selected }}>{text}</h1>
+                    <h1 style={{ color, fontFamily: selected, fontSize: size + 'em'}}>{text}</h1>
                     <img src={image}/>
                 </div>
                 <div>
